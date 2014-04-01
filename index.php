2e6f6a2d5a4ca12856908dccc6a4e0276f4c130d
daniel <daniel@garganiel>
2014-04-01 19:44:37 +0200

    1er commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "<h1>daniel ha estado aqui</h1>";
+        ?>
+    </body>
+</html>
